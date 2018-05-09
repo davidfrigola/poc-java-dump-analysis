4003bc35615e4cffb0a2ccce9ffec78a4c8d2e35
david.frigola <david.frigola@gmail.com>
2018-05-09 20:05:18 +0200

    #9 Parameters for doThread

--- app/src/main/java/es/davidfrigola/poc/heapanalysis/app/ThreadNightmareController.java
+++ app/src/main/java/es/davidfrigola/poc/heapanalysis/app/ThreadNightmareController.java
@@ -5,23 +5,28 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ThreadNightmareController {
 
-    private static final long DELAY_MS = TimeUnit.MINUTES.toMillis(1);
+    private static final Long DELAY_MS = TimeUnit.MINUTES.toMillis(1);
     
     private static final String THREAD_NAME_PREFIX =  "ThreadsNighmare";
+	private static final String DEFAULT_DELAY_MS = "60000";
+
+	private static final String DEFAULT_THREADS = "1000";
 
     @RequestMapping("/doThread")
-    public String doThread(){
+    public String doThread(@RequestParam(name="delay",defaultValue=DEFAULT_DELAY_MS) Long delay,
+    		@RequestParam(name="threads",defaultValue=DEFAULT_THREADS) Integer threads){
 
-        for(int i=0;i<1000;i++){
+        for(int i=0;i<threads;i++){
             TaskExecutor executor = new SimpleAsyncTaskExecutor(THREAD_NAME_PREFIX);
             executor.execute(() -> {
                 try {
-                    Thread.sleep(DELAY_MS);
+                    Thread.sleep(delay);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
